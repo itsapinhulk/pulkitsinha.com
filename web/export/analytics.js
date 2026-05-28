69d2e68f1f7896882074f345c78da495aa9231a4
Pulkit Sinha <itsapinhulk@outlook.com>
2026-05-27 22:59:28 -0400

    Update Google Analytics tag

--- web/export/analytics.js
+++ web/export/analytics.js
@@ -1,11 +1,11 @@
 (function() {
   var s = document.createElement('script');
   s.async = true;
-  s.src = 'https://www.googletagmanager.com/gtag/js?id=G-RYQE3KZB0M';
+  s.src = 'https://www.googletagmanager.com/gtag/js?id=G-V02BNBV1QX';
   document.head.appendChild(s);
 
   window.dataLayer = window.dataLayer || [];
   function gtag() { dataLayer.push(arguments); }
   gtag('js', new Date());
-  gtag('config', 'G-RYQE3KZB0M');
+  gtag('config', 'G-V02BNBV1QX');
 })();
